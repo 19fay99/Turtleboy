532527b132823522bba299b6512ab550583db3f0
Andrew Fay Matseas <andrewafay@gmail.com>
2022-04-07 16:51:33 -0400

    Added Level Scripts
    When the school is reached on each level, the next level is loaded

---
+++ Turtleboy/Assets/Scripts/Level Complete/Level1.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Level1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Turtleboy")
+        {
+            SceneManager.LoadScene("Level 2");
+        }
+    }
+}
